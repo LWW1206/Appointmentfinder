f531426c17a592d25040cc51f94ab2635a566046
Matthias Kemmer <126191759+matthiaskemmer@users.noreply.github.com>
2023-04-21 18:11:43 +0200

    Remove unused files

--- frontend/includes.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'include/header.php';
-require 'include/config.php'; //comment out to see error msges
-include_once 'include/navigation.php';
-include_once 'include/navbar.php';
-include_once 'include/footer.php';
-?>
